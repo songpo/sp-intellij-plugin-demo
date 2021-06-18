55e5afbf6dd1dae40ee2bd5f9984cde3054f6d2c
GitHub Action <action@github.com>
2021-06-18 15:04:04 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/songpo/spintellijplugindemo/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.songpo.spintellijplugindemo.services
+
+import com.github.songpo.spintellijplugindemo.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
